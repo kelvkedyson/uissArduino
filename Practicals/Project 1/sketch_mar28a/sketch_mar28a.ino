34ffeaf3b367df341217895651469852ca575b21
kelvkedyson <kelvinkedysonzacharia@gmail.com>
2021-03-28 21:11:41 +0300

    Changed to Project 1

---
+++ Practicals/Project 1/sketch_mar28a/sketch_mar28a.ino
@@ -0,0 +1,40 @@
+/*
+  Written by: Nelson Kimaro
+  BSC Electronics Science and Communications (UDSM - CoICT)
+  March 28, 2021
+*/
+
+int LDR = A0; //using analog pin A0 for LDR
+int LED = 8; //using digital pin 8 for LED
+
+//configuration codes (code that run only once)
+void setup() {
+  
+  //initializing the serial monitor with 9600 baudrate
+  Serial.begin(9600);
+
+  //setting LDR analog pin as INPUT
+  pinMode(LDR, INPUT);
+
+  //setting LED digital pin as OUTPUT
+  pinMode(LED, OUTPUT);
+  
+}
+
+//all time running method (code that run repeatedly)
+void loop() {
+  
+  float sensorValue = analogRead(LDR); //getting the values of the LDR from the analog pin whenever there is a change
+
+  //printint the values on the serial monitor
+  Serial.print("Sensor Value : ");
+  Serial.println(sensorValue);
+
+  //controlling the LED lighting
+  if(sensorValue > 1000){
+    digitalWrite(LED, HIGH); //switching ON the light
+  }else{
+    digitalWrite(LED, LOW); //switching OFF the light
+  }
+  
+}
